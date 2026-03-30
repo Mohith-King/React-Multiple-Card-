ef728159320aa59871892c37ff3a0eb840ec84bc
Mohith Naik <mohithnaik212@gmai.com>
2026-03-30 14:37:25 +0530

    Update Card component props

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,25 +1,25 @@
 import { BookmarkIcon } from "lucide-react";
 
-const Card = (props) => {
+const Card = ({brandLogo,company,datePosted,post,tag1,tag2,pay}) => {
   return (
    <div className="card">
             <div>
                 <div className="top">
-                    <img src={props.brandLogo} alt="nachioooooo" />
+                    <img src={brandLogo} alt="nachioooooo" />
                     <button>Save <BookmarkIcon size={10} /> </button>
                 </div>
                 <div className="center">
-                    <h3>{props.company} <span>{props.datePosted}</span></h3>
-                    <h2>{props.post}</h2>
+                    <h3>{company} <span>{datePosted}</span></h3>
+                    <h2>{post}</h2>
                     <div className='tag'>
-                        <h4>{props.tag1}</h4>
-                        <h4>{props.tag2}</h4>
+                        <h4>{tag1}</h4>
+                        <h4>{tag2}</h4>
                     </div>
                 </div>
             </div>
             <div className="bottom">
                 <div>
-                    <h3>{props.pay}</h3>
+                    <h3>{pay}</h3>
                     <p>Mumbai, India</p>
                 </div>
                 <button>Apply Now</button>
